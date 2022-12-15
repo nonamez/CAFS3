5f3e3f638f2beeef82b8c1a5cf75bf1f1c6d9c93
Kiril <hello@nonamez.name>
2022-12-15 15:18:12 +0200

    misc

--- FrontEnd/JavaScrit/2022-12-13/dom-elements-manipulations/assets/app-V3.js
+++ FrontEnd/JavaScrit/2022-12-13/dom-elements-manipulations/assets/app-V3.js
@@ -2,7 +2,6 @@ const SVG_PATH_TRASH = 'M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5
 const SVG_PATH_PENCIL = 'M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z';
 const SVG_PATH_BELL = 'M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z';
 
-
 function createTdWithText(text) {
 	const td = document.createElement('td');
 
@@ -30,7 +29,6 @@ function getTrashButton() {
 
 	return trashButton;
 }
-
 
 function getActions() {
 	const td = document.createElement('td');
@@ -62,6 +60,7 @@ function getBootstrapIcon(pathValue) {
     const svg = document.createElementNS(xmlns, 'svg');
 
     // https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute
+    // https://developer.mozilla.org/en-US/docs/Web/API/Element/getAttribute
     svg.setAttribute('width', 16);
     svg.setAttribute('height', 16);
     svg.setAttribute('viewBox', '0 0 16 16');
