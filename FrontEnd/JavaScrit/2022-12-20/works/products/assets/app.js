76511e0dffa9b67bbaffe0732234b500d0acf920
Kiril <hello@nonamez.name>
2022-12-20 22:00:38 +0200

    misc

--- FrontEnd/JavaScrit/2022-12-20/works/products/assets/app.js
+++ FrontEnd/JavaScrit/2022-12-20/works/products/assets/app.js
@@ -22,14 +22,53 @@ function createTdWithText(text) {
     return td;
 }
 
-getPostsViaFetch().then(data => {
-    for (let post of data) {
+const users = {};
+
+// getPostsViaFetch().then(async data => {
+//     for (let post of data) {
+//         const tr = document.createElement('tr');
+//         postsTbody.appendChild(tr);
+
+//         tr.appendChild(createTdWithText(post.id));
+//         tr.appendChild(createTdWithText(post.title));
+//         tr.appendChild(createTdWithText(post.body));
+
+//         try {
+//             if (post.userId in users == false) {
+//                 let response = await fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`);
+
+//                 users[post.userId] = await response.json();
+//             }
+
+//             tr.appendChild(createTdWithText(users[post.userId].name));
+//         } catch(err) {
+//             console.error(err);
+//         }
+//     }
+// });
+
+(async () => {
+    const posts = await getPostsViaFetch();
+
+    for (let post of posts) {
         const tr = document.createElement('tr');
+        
         postsTbody.appendChild(tr);
 
         tr.appendChild(createTdWithText(post.id));
         tr.appendChild(createTdWithText(post.title));
         tr.appendChild(createTdWithText(post.body));
-        tr.appendChild(createTdWithText(post.userId));
+
+        try {
+            if (post.userId in users == false) {
+                let response = await fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`);
+
+                users[post.userId] = await response.json();
+            }
+
+            tr.appendChild(createTdWithText(users[post.userId].name));
+        } catch(err) {
+            console.error(err);
+        }
     }
-});
+})();
